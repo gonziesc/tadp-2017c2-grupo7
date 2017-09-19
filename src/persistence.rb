fd464caad55aca97afb2203a675d89cf5b147699
gonziesc <gonziesc@hotmail.com>
2017-09-18 21:36:58 -0300

    refactoring persistable fields

--- src/persistence.rb
+++ src/persistence.rb
@@ -10,9 +10,10 @@ module Persistence
   module ClassPersistence
     attr_accessor :persistable_fields
     def has_one type, hash
-      @name = hash[:named]
-      attr_accessor(@name)
-      self.persistable_fields ? self.persistable_fields.push(@name) : self.persistable_fields = [@name]
+      name = hash[:named]
+      attr_accessor(name)
+      @persistable_fields ||= []
+      @persistable_fields << name
     end
   end
 
