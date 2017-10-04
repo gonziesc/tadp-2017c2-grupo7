be8a777035659c9139f61084bf1f1fa7b8243664
juanimoli <juanignaciomoli@gmail.com>
2017-10-04 20:25:21 -0300

    Punto 1 y 2 resueltos

--- lib/persistence.rb
+++ lib/persistence.rb
@@ -9,10 +9,13 @@ module Persistence
   end
 
   module ClassPersistence
-    attr_accessor :sticky_fields, :table
+    attr_accessor :primitive_sticky_fields, :table, :complex_sticky_fields
+    def primitive_sticky_fields
+      @primitive_sticky_fields ||= { id: String }
+    end
 
-    def sticky_fields
-      @sticky_fields ||= { id: String }
+    def complex_sticky_fields
+      @complex_sticky_fields ||= {}
     end
 
     def table
@@ -20,43 +23,52 @@ module Persistence
     end
 
     def has_one (type, hash)
+      primitive?(type) ? primitive_sticky_fields[hash[:named]] = type : complex_sticky_fields[hash[:named]] = type
       attr_accessor(hash[:named])
-      sticky_fields[hash[:named]] = type
     end
 
     def save!(instance)
-      id = table.upsert(instance)
-      instance.instance_variable_set("@id", id)
+      complex_sticky_fields.each do |name, _|
+        instance.send(name).save!
+      end
+      instance.id = table.upsert(instance)
     end
 
     def forget!(instance)
       table.delete(instance.id)
-      instance.instance_variable_set("@id", nil)
+      instance.id = nil
     end
 
     def refresh!(instance)
-        savedInstance = table.entries.find{ |i| i[:id] == instance.id }
-        sticky_fields.each do
-        |name, type| instance.instance_variable_set("@#{name}", savedInstance[name])
-        end
+      saved_instance_hash = table.entries.find{ |i| i[:id] == instance.id }
+      primitive_sticky_fields.each do |name, _|
+        instance.send("#{name}=", saved_instance_hash[name])
+      end
+      complex_sticky_fields.each do |name, type|
+        sub_instance = type.new
+        sub_instance.id = saved_instance_hash[name]
+        sub_instance.refresh!
+        instance.send("#{name}=", sub_instance)
+      end
     end
 
     def to_hash(instance)
       hash = {}
-      sticky_fields.each {|name, type| hash[name] = instance.instance_variable_get("@#{name}")}
+      primitive_sticky_fields.each { |name, _| hash[name] = instance.send(name)}
+      complex_sticky_fields.each { |name, _| hash[name] = instance.send(name).id}
       hash
     end
 
     def all_instances
       table = TADB::DB.table(self.name)
-      instances = table.entries.map { |instance|  (create_new_instance instance) }
+      instances = table.entries.map { |instance| (create_new_instance instance) }
       instances
     end
 
     def create_new_instance (attributes)
       instance = self.new
-      attributes.each do
-      |name, value| instance.instance_variable_set("@#{name}", value)
+      attributes.each do |name, value|
+        instance.send("#{name}=", value)
       end
       instance
     end
@@ -83,6 +95,14 @@ module Persistence
       end
     end
 
+    def primitive?(type)
+      (type == String) || (type == Integer) || (type == Boolean)
+    end
+
+    def attribute_exists?(attribute)
+      primitive_sticky_fields.keys.any? { |key| key == attribute} || complex_sticky_field.keys.any? {|key| key == attribute}
+    end
+
   end
 
   module InstancePersistence
@@ -98,7 +118,8 @@ module Persistence
     end
 
     def refresh!
-      raise("Este objeto no tiene id!")
+      # raise("Este objeto no tiene id!")
+      self.class.refresh!(self)
     end
 
     def forget!
@@ -108,6 +129,15 @@ module Persistence
     def to_hash
       self.class.to_hash(self)
     end
+
+    def primitive_sticky_fields
+      self.class.primitive_sticky_fields
+    end
+
+    def complex_sticky_fields
+      self.class.complex_sticky_fields
+    end
+
   end
 end
 
@@ -115,7 +145,7 @@ end
 module TADB
   class Table
     def upsert(object)
-      if (persisted? object)
+      if persisted? object
         update object
       else
         insert(object.to_hash)
@@ -128,7 +158,31 @@ module TADB
     end
 
     def persisted?(object)
-      entries.any? { |(key, value)| key == "id" and value = object.id }
+      entries.any? { |(key, value)| key == "id" && value == object.id }
     end
   end
 end
+
+class Auto
+  include Persistence
+  has_one String, named: :marca
+end
+
+class Persona
+  include Persistence
+  has_one String, named: :nombre
+  has_one Integer, named: :edad
+  has_one Auto, named: :auto
+end
+
+pers_a = Persona.new
+pers_b = Persona.new
+pers_a.nombre = "Pedro"
+pers_a.edad = 10
+pers_a.auto = Auto.new
+pers_a.auto.marca = "Audi"
+pers_a.save!
+pers_b.id = pers_a.id
+pers_b.refresh!
+puts pers_b.to_hash
+puts pers_b.auto.marca
